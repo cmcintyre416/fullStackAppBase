991c8b60ef1dc7ac861c50abd2e7112551d0d192
Colin Mcintyre <colin@colinmcintyre.io>
2018-12-20 20:26:03 -0500

    remove heroku add prox

--- services/passport.js
+++ services/passport.js
@@ -21,7 +21,7 @@ passport.use(
     {
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
-        callbackURL: 'https://morning-savannah-97535.herokuapp.com/auth/google/callback',
+        callbackURL: '/auth/google/callback',
         proxy: true
     }, 
     (accessToken, refreshToken, profile, done) => {
